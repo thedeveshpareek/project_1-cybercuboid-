86d5dd33239869553c74b150a4f718838b9e32a2
Aashutoshsharma002 <aashutoshsharma568@gmail.com>
2022-01-05 12:03:12 +0000

    Create AllSvg Subcomponent @Aashutoshsharma002

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {Svg1} from './subcomponents/AllSvg';
 
 function App() {
   return (
     <div className="App">
-        started
+        started Here
     </div>
   );
 }
