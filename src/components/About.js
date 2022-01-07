f40aa563e2a04aa2ac6a6803bc332053ae197a89
Devesh pareek <thedeveshpareek@gmail.com>
2022-01-07 10:08:58 +0000

    @thedeveshpareek

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <>
+            <div>
+                <div>image is here</div>
+                <div>
+                    <div>
+                        <h1>About Project</h1>
+                        <p>
+                            We have you covered no matter where you are located
+                        </p>
+                    </div>
+                    <div>Character</div>
+                </div>
+            </div>
+        </>
+    )
+}
