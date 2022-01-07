9b777d2bb2f86d738d03e6d143ad55df8e282906
Devesh pareek <thedeveshpareek@gmail.com>
2022-01-07 12:00:53 +0000

    @thedeveshpareek

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { About } from './components/About';
 
 function App() {
   return (
     <div className="App">
-        started Here
+        <About/>
     </div>
   );
 }
